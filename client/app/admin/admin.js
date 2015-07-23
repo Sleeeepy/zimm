153f9b279ba404981207906c6936950f2a4f587d
Sleeeepy <elmarzimmermann@hotmail.com>
2015-07-23 09:48:09 +0100

    initial commit

---
+++ client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('zimmApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
