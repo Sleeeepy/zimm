703531d37e717fd4331569a6291815b183dfc879
Sleeeepy <elmarzimmermann@hotmail.com>
2015-07-23 20:49:37 +0100

    added basic routes/endpoints

--- server/config/socketio.js
+++ server/config/socketio.js
@@ -18,6 +18,8 @@ function onConnect(socket) {
   });
 
   // Insert sockets below
+  require('../api/chat/chat.socket').register(socket);
+  require('../api/message/message.socket').register(socket);
   require('../api/thing/thing.socket').register(socket);
 }
 
